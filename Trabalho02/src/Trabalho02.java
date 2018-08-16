fe2672a83eb31ebd62ab7e124ae672d9f9e73758
LeoWPereira <leonardo.wpereira@hotmail.com>
2018-08-16 20:48:47 -0300

    Add the very first .java file

---
+++ Trabalho02/src/Trabalho02.java
@@ -0,0 +1,11 @@
+public class Trabalho02 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Bem-vinda professora Ana Cristina!");
+		
+		System.out.println("\nVamos começar o trabalho 02?\n");
+		
+		return;
+	}
+}
