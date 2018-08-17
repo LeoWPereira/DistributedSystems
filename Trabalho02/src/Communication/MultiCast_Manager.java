d34d75a5e794c5540eda8647ad24bdeb9904b2ae
LeoWPereira <leonardo.wpereira@hotmail.com>
2018-08-16 22:37:57 -0300

    #2: Add first part of the solution

--- Trabalho02/src/Communication/MultiCast_Manager.java
+++ Trabalho02/src/Communication/MultiCast_Manager.java
@@ -11,13 +11,109 @@
 
 package Communication;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.net.SocketException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
- * @name	MultiCast_Manager
+ * @name MultiCast_Manager
  * @brief
  * 
  *
  */
-public class MultiCast_Manager 
+public class MultiCast_Manager extends Thread 
 {
+	/**
+	 * @name
+	 * @brief
+	 */
+	private int communicationPort;
+	
+	/**
+	 * @name
+	 * @brief
+	 */
+	private InetAddress communicationGroup;
+	
+	/**
+	 * @name
+	 * @brief
+	 */
+	private MulticastSocket socket;
+	
+	/**
+	 * @name
+	 * @brief
+	 */
+	public MultiCast_Manager(int _communicationPort,
+							 String _communcationGroup)
+	{
+		this.communicationPort 	= _communicationPort;
+		
+		try
+		{
+			this.communicationGroup = InetAddress.getByName(_communcationGroup);
+			
+			socket					= new MulticastSocket(_communicationPort);
+			
+			socket.joinGroup(this.communicationGroup);
+		}
+		
+		catch(SocketException e)
+		{
+			System.out.println("Socket: " + e.getMessage());
+		}
+		
+		catch(IOException e)
+		{
+			System.out.println("IO: " + e.getMessage());
+		}
+		
+		return;
+	}
 
+	/**
+	 * @name
+	 * @brief
+	 */
+	public void run() 
+	{
+		return;
+	}
+
+	/**
+	 * @name
+	 * @brief
+	 * @param _message
+	 * @return
+	 */
+	public boolean sendMessage(byte[] _message) 
+	{
+		boolean returnValue = false;
+
+		DatagramPacket messageOut = new DatagramPacket(_message, 
+													   _message.length, 
+													   this.communicationGroup, 
+													   this.communicationPort);
+
+		try 
+		{
+			this.socket.send(messageOut);
+			
+			returnValue = true;
+		}
+		
+		catch (IOException ex) 
+		{
+			Logger.getLogger(MultiCast_Manager.class.getName()).log(Level.SEVERE, 
+																	null, 
+																	ex);
+		}
+
+		return returnValue;
+	}
 }
--- Trabalho02/src/Trabalho02.java
+++ Trabalho02/src/Trabalho02.java
@@ -9,16 +9,143 @@
  ******************************************************************************
  */
 
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
 import Communication.MultiCast_Manager;
 
 public class Trabalho02 
 {
-	public static void main(String[] args) 
+	/**
+	 * @name
+	 * @brief
+	 */
+	private static Scanner scanKeyboard = new Scanner(System.in);
+	
+	/**
+	 * @name
+	 * @brief
+	 */
+	public static int communicationPort = 6689;
+	
+	/**
+	 * @name
+	 * @brief
+	 */
+	public static String communicationGroup = "224.0.0.10";
+	
+	/**
+	 * @name
+	 * @brief
+	 */
+	public static MultiCast_Manager multiCast;
+	
+	/**
+	 * @name
+	 * @brief
+	 * @param args
+	 */
+	public static void main(String[] args) throws InterruptedException 
 	{
 		System.out.println("Bem-vinda professora Ana Cristina!");
 		
 		System.out.println("\nVamos começar o trabalho 02?\n");
 		
+		configPort();
+		
+		configGroup();
+		
+		if(testMultiCastSocket())
+		{
+			
+		}
+		
 		return;
+	}
+	
+	/**
+	 * @name	
+	 * @brief	
+	 * @return
+	 */
+	public static void configPort()
+	{
+		int port = 0;
+		
+		System.out.println("Primeiro: Qual porta será utilizada para a comunicação (O padrão é 6689)?");
+		System.out.println("Caso queira manter o padrão, digite 0");
+		
+		port = scanKeyboard.nextInt();
+		
+		if(0 != port)
+		{
+			communicationPort = port;
+		}
+		
+		return;
+	}
+	
+	/**
+	 * @name	
+	 * @brief	
+	 * @return
+	 */
+	public static void configGroup()
+	{
+		boolean validOption = false;
+		
+		String group = "";
+		
+		System.out.println("\nSegundo: Qual será o endereço IP utilizado pelo MultiCast (O padrão é 224.0.0.10)?");
+		System.out.println("Caso queira manter o padrão, digite 0");
+		
+		while(!validOption)
+		{
+			group = scanKeyboard.nextLine();
+			
+			if((!group.equals("")) || (group.equals("0")))
+			{
+				if(!group.equals("0"))
+				{
+					communicationGroup = group;
+				}
+				
+				validOption = true;
+			}
+		}
+		
+		return;
+	}
+
+	/**
+	 * @name	
+	 * @brief	
+	 * @return
+	 */
+	public static boolean testMultiCastSocket() throws InterruptedException
+	{
+		boolean returnValue = false;
+		
+		int tempoRestante = 2;
+		
+		multiCast = new MultiCast_Manager(communicationPort, 
+										  communicationGroup);
+		
+		multiCast.start();
+		
+		System.out.println("\nMuito obrigado! Aguarde alguns instantes para que eu possa testar suas configurações");
+		
+		while(0 < tempoRestante)
+		{
+			TimeUnit.SECONDS.sleep(1);
+			
+			System.out.println(".\n");
+			
+			tempoRestante--;
+		}
+		
+		
+		
+		return returnValue;
 	}
 }
